b5d36df4766fb15ec291eeef68bc5d25786f5cfc
Debiprasad Das <debiprasaddaskdv@gmail.com>
2020-11-19 09:01:33 +0530

    update bot.py

--- bot.py
+++ bot.py
@@ -2,22 +2,22 @@ import telebot
 import time
 
 
-bot_token='your_token'
+bot_token='your_bot-token'
 
   
 bot=telebot.TeleBot(token=bot_token)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-  bot.reply_to(message,"Hi There Welcome!🤗      I can echo your message,type any message 😉🎤")
+  bot.reply_to(message,"Hi There😃 Welcome!🤗.......I can echo your message,type any message 😉🎤")
  
 @bot.message_handler(commands=['yt'])
 def send_welcome(message):
-  bot.reply_to(message,'Visit here!🔥    https://www.youtube.com/channel/UCcMTl3AlK6NSxN8on0tZv_Q')
+  bot.reply_to(message,'Visit here!..... https://www.youtube.com/channel/UCcMTl3AlK6NSxN8on0tZv_Q')
  
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
-  bot.reply_to(message,'Contact my master❤️🖤 @DebNationXD for any support that you need              Thank You 🙏')
+  bot.reply_to(message,'Contact my piro owner❤️😂... @DebNationXD for any support that you need...........Thank You 🙏')
   
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
